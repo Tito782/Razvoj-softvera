d0a2a25e78bb29f05f48ca943dc290b28da84594
Tito782 <vitor2d282gmail.com>
2022-02-05 11:54:53 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+      <>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' exact />
+          </Routes>
+        </Router>
+      </>
+  );
+}
+
+export default App;
---
+++ src/components/Sekcija.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Sekcija() {
+  return (
+        <div className='hero-conatiner'>
+            <video src = '/videos/video-2.mp4'></video>
+        </div>
+        );
+}
+
+export default Sekcija;
