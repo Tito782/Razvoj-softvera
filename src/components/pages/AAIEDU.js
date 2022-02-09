83257721b254e1fd93e6c42876dead673b77e9cc
Tito782 <47535133+Tito782@users.noreply.github.com>
2022-02-09 14:14:26 +0100

    Update AAIEDU.js

--- src/components/pages/AAIEDU.js
+++ src/components/pages/AAIEDU.js
@@ -23,9 +23,9 @@ function AAIEDU() {
 					<input type="checkbox" id="toggle-password"/>
 					<label for="toggle-password">Pokaži zaporku</label>
 				</div>
-				<button type="submit" class="login-btn" aria-label="Kliknite ovdje za prijavu svojim e-identitetom iz sustava AAI@EduHr" tabindex="3" name="Submit"><a get="https://www.aaiedu.hr/pomoc">PRIJAVA</a></button>
+				<button href="/" type="submit" class="login-btn" aria-label="Kliknite ovdje za prijavu svojim e-identitetom iz sustava AAI@EduHr" tabindex="3" name="Submit"><a get="https://www.aaiedu.hr/pomoc">PRIJAVA</a></button>
 				<div class="bottom-links">
-					<p><a href="https://www.aaiedu.hr/pomoc" title="Kliknite ovdje za pomoć." target="_blank">Pomoć</a></p>
+					<p><a href="https://www.aaiedu.hr/pomoc" title="Kliknite ovdje za pomoć." target="_blank" rel="noreferrer">Pomoć</a></p>
 					<small class="version">Pišo v3.0</small>
 				</div>
 			</form>
