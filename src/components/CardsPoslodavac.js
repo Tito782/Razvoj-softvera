0b7ff0e1014cb5b720a1acf3eff4124213637b55
Bepino <yak789456.89@gmail.com>
2022-02-24 00:59:09 +0100

    sutra ce biti bolje

--- src/components/CardsPoslodavac.js
+++ src/components/CardsPoslodavac.js
@@ -5,25 +5,63 @@ import Oglasi from './data/Oglasi.json';
 
 function Cards() {
 
-  var showExtra = true
-
   function setExtra(i) {
-    if(showExtra)
-      document.getElementById(i).style.display = 'block';
+    if(document.getElementById(i).style.display !== 'flex')
+      document.getElementById(i).style.display = 'flex';
     else
       document.getElementById(i).style.display = 'none';
-
-    showExtra = !showExtra;
   } 
+
+  function ClickButtonOn() {
+    document.getElementById('QuickAddMenu').style.display = 'flex';
+  }
+
+  function ClickButtonOff() {
+    document.getElementById('QuickAddMenu').style.display = 'none';
+  }
 
   return (
     <div className='cards'>
+      <div id='QuickAddMenu' style={{'display':'none'}}>
+          <div className='QA_container'>
+            <button onClick={ClickButtonOff}>rtt</button>
+          </div>
+        </div>
+      <div className='quickadd_bar'>
+        <button className='botun_add notClicked' onClick={ClickButtonOn}>Add listing</button>
+        <div className='filteri'>
+          <select className='botun_filteri' id='d'>
+            <option value='dogo'>Poslodavci..</option>
+            <option value='dogo'>Hoteli d.d.</option>
+            <option value='dogo'>Sonjini Cvjetovi d.o.o</option>
+            <option value='dogo'>Mirko d.o.o</option>
+            <option value='dogo'>Ehefrauen-Sohn GmbH</option>
+            <option value='dogo'>Gary's software Inc.</option>
+          </select>
+          <select className='botun_filteri' id='e'>
+            <option value='dogo'>Gradovi..</option>
+            <option value='dogo'>Zagreb</option>
+            <option value='dogo'>Zadar</option>
+            <option value='dogo'>Split</option>
+            <option value='dogo'>Rijeka</option>
+            <option value='dogo'>Pula</option>
+          </select>
+          <select className='botun_filteri' id='n'>
+            <option value='dogo'>Sektor..</option>
+            <option value='dogo'>Hoteljerstvo-i-ugostiteljstvo</option>
+            <option value='dogo'>Elektrotehnika</option>
+            <option value='dogo'>Cvjećarstvo</option>
+            <option value='dogo'>Vodoinstalacije</option>
+            <option value='dogo'>IT</option>
+          </select>
+        </div>
+      </div>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             {Oglasi.map(element => {
-              return (<div onClick={() => setExtra(element.key)}>
-                <CardItem 
+              return (<div><div onClick={() => setExtra(element.key)}>
+                <CardItem
                   key = {element.key}
                   src={'../' + element.Slika}
                   text= {element.naslov}
@@ -33,8 +71,11 @@ function Cards() {
                   grad = {element.grad}
                   autor = {element.autor}>
                 </CardItem>
+                </div>
                 <div className='extra__info' id={`${element.key}`} style={{display:'none'}}>
-                  <div className='text__opis'>{element.Opis}</div>
+                  <div className='tekst opis'>{element.Opis}</div>
+                  <div className='tekst vrsta'> Ponuda: {element.ponuda}</div>
+                  <div className='tekst vrsta'> Sektor: {element.Vrsta}</div>
                 </div>
               </div>);
             })}
