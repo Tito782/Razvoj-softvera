b4d9b4ec36bc0df7a79877290cabd7ec7f31f95d
Tito782 <vitor2d282gmail.com>
2022-02-24 17:04:30 +0100

    commit

--- src/components/pages/Ucenik.js
+++ src/components/pages/Ucenik.js
@@ -14,7 +14,7 @@ function Ucenik(){
         <div class="container rounded bg-white mt-5">
     <div class="row">
         <div class="col-md-4 border-right">
-            <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" src="https://i.imgur.com/0eg0aG0.jpg" width="90"/><span class="font-weight-bold">John Doe</span><span class="text-black-50">john_doe12@bbb.com</span><span>United States</span></div>
+            <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" src="https://i.imgur.com/0eg0aG0.jpg" width="90" alt=""/><span class="font-weight-bold">John Doe</span><span class="text-black-50">john_doe12@bbb.com</span><span>United States</span></div>
         </div>
         <div class="col-md-8">
             <div class="p-3 py-5">
